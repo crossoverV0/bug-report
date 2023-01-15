87453201357ea81fd7c73bd70749dc761073fc8e
Tiago Menezes <tiagomenezes013@gmail.com>
2023-01-15 18:45:39 -0300

    adequando elementos

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,20 +8,20 @@ import { PagesComponent } from './pages.component';
 import {ButtonModule} from 'primeng/button';
 import { TieredMenuModule } from 'primeng/tieredmenu';
 import {MenuItem} from 'primeng/api';
-import { ReportFormComponent } from './report-form/report-form.component';
 import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
 import {FormsModule} from '@angular/forms';
 import { ReportsComponent } from './reports/reports.component';
+import {TableModule} from 'primeng/table';
+
 
 @NgModule({
   declarations: [
     PagesComponent,
-    ReportFormComponent,
     NotFoundComponent,
     ReportsComponent,
   ],
   imports: [
-  TieredMenuModule,
+    TableModule,
     CommonModule,
     ButtonModule,
     PagesRoutingModule,
