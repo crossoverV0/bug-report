35047f48865e2c5eddd18d89357cc554869c2d2d
Tiago Menezes <tiagomenezes013@gmail.com>
2023-02-25 14:45:57 -0300

    reestruturando projeto

--- src/app/pages/pages-routing.module.ts
+++ src/app/pages/pages-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { NotFoundComponent } from '../shared/not-found/not-found.component';
 import { PagesComponent } from './pages.component';
 import { ReportsComponent } from './reports/reports.component';
 
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,16 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { SharedModule } from './shared/shared.module';
-
 
 import { PagesRoutingModule } from './pages-routing.module';
 import { PagesComponent } from './pages.component';
 import {ButtonModule} from 'primeng/button';
 import { TieredMenuModule } from 'primeng/tieredmenu';
 import {MenuItem} from 'primeng/api';
-import { NotFoundComponent } from './miscellaneous/not-found/not-found.component';
+import { NotFoundComponent } from '../shared/not-found/not-found.component';
 import {FormsModule} from '@angular/forms';
-import { ReportsComponent } from './reports/reports.component';
 import {TableModule} from 'primeng/table';
 import {SplitButtonModule} from 'primeng/splitbutton';
 
@@ -18,7 +15,6 @@ import {SplitButtonModule} from 'primeng/splitbutton';
   declarations: [
     PagesComponent,
     NotFoundComponent,
-    ReportsComponent,
   ],
   imports: [
     SplitButtonModule,
@@ -27,7 +23,6 @@ import {SplitButtonModule} from 'primeng/splitbutton';
     ButtonModule,
     PagesRoutingModule,
     FormsModule,
-    SharedModule
   ]
 })
 export class PagesModule { }
